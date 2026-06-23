01249a3c1782d1f393f3ed218c9abf597aab682e
VkH_758 <vkhyzhniak496@gmail.com>
2026-06-23 21:50:41 +0200

    Wersja stabilna przed snem

--- server/src/main/kotlin/com/example/volyna_tram/Application.kt
+++ server/src/main/kotlin/com/example/volyna_tram/Application.kt
@@ -1,20 +1,20 @@
-package com.example.volyna_tram
+        package com.example.volyna_tram
 
-import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+        import io.ktor.server.application.*
+        import io.ktor.server.engine.*
+        import io.ktor.server.netty.*
+        import io.ktor.server.response.*
+        import io.ktor.server.routing.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
-
-fun Application.module() {
-    routing {
-        get("/") {
-            call.respondText(sayHello("Ktor"))
+        fun main() {
+            embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+                .start(wait = true)
         }
-    }
-}
+
+        fun Application.module() {
+            routing {
+                get("/") {
+                    call.respondText(sayHello("Ktor"))
+                }
+            }
+        }
