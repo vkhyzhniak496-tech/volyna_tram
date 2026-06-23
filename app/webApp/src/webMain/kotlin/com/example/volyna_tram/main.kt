dbe89b4ac2a600bab4b4f59eedae2d38e5795bdc
VkH_758 <vkhyzhniak496@gmail.com>
2026-06-23 21:34:39 +0200

    Ktor aka ctor, aka Thor

---
+++ app/webApp/src/webMain/kotlin/com/example/volyna_tram/main.kt
@@ -0,0 +1,11 @@
+package com.example.volyna_tram
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport {
+        App()
+    }
+}
---
+++ core/src/commonMain/kotlin/com/example/volyna_tram/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.example.volyna_tram
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
